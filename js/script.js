ada62857ae52e0f70f46c7b7eb31565017233c8c
JFernando <almo_fernando@hotmail.com>
2024-05-02 14:33:28 -0300

    final commits

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe-game');
 const bigPipe = document.querySelector('.pipe-game-2');
@@ -44,6 +45,7 @@ const gameLevel = setInterval(() => {
   const pipePosition = pipe.offsetLeft;
   if (fixedCont == 19.35) {
     pipe.style.animation = 'pipe-animation-score 1.2s infinite linear';
+    //pipe.style.left = `${pipePosition}px` ;
   }
 
   if (fixedCont == 19.95) {
@@ -56,6 +58,7 @@ const gameLevel = setInterval(() => {
 
 //configurações gerais do jogo
 const loopGame = setInterval(() => {
+
   const pipePosition = pipe.offsetLeft;
   const bigPipePosition = bigPipe.offsetLeft;
   const bulletPosition = bullet.offsetLeft;
@@ -63,27 +66,147 @@ const loopGame = setInterval(() => {
   const birdPosition = bird.offsetLeft;
   const cloudsPosition = clouds.offsetLeft;
   const enemyPosition = enemy.offsetLeft;
-  const marioPosition = +window
-    .getComputedStyle(mario)
-    .bottom.replace('px', '');
+  const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+  //condicional para parar as animações - game over
 
   //bloco game over small pipe
   if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-    gameOverLogic();
+
+    //parando as animações
+      pipe.style.animation = 'none';
+      pipe.style.left = `${pipePosition}px`;
+
+      mario.style.animation = 'none';
+      mario.style.bottom = `${marioPosition}px`;
+
+      bigPipe.style.animation = 'none';
+      bigPipe.style.left = `${bigPipePosition}px`;
+
+      bullet.style.animation = 'none';
+      bullet.style.left = `${bulletPosition}px`;
+
+      bulletTwo.style.animation = 'none';
+      bulletTwo.style.left = `${bulletTwoPosition}px`;
+
+      bird.style.animation = 'none';
+      bird.style.left = `${birdPosition}px`;
+
+      clouds.style.animation = 'none';
+      clouds.style.left = `${cloudsPosition}px`;
+
+      enemy.style.animation = 'none';
+      enemy.style.left = `${enemyPosition}px`;
+
+      firstGrass.style.animation = 'none';
+      secondGrass.style.animation = 'none';
+
+      //substituição do mario
+      mario.src = 'assets/imgs/mario-game-over.png';
+      mario.style.width = '75px';
+      mario.style.marginLeft = '45px';
+      marioTheme.src = 'assets/audio/smb_gameover.mp3';
+
+      //display elements
+      gameOver.style.display = 'block';
+      restart.style.display = 'block';
+
+
+        clearInterval(loopGame);
   }
 
   //bloco game over big pipe
-  else if (
-    bigPipePosition <= 120 &&
-    bigPipePosition > 0 &&
-    marioPosition < 80
-  ) {
-    gameOverLogic();
+  else if (bigPipePosition <= 120 && bigPipePosition > 0 && marioPosition < 80) {
+
+    //parando animações
+    pipe.style.animation = 'none';
+    pipe.style.left = `${pipePosition}px`;
+
+    mario.style.animation = 'none';
+    mario.style.bottom = `${marioPosition}px`;
+
+    bigPipe.style.animation = 'none';
+    bigPipe.style.left = `${bigPipePosition}px`;
+
+    bullet.style.animation = 'none';
+    bullet.style.left = `${bulletPosition}px`;
+
+    bulletTwo.style.animation = 'none';
+    bulletTwo.style.left = `${bulletTwoPosition}px`;
+
+    bird.style.animation = 'none';
+    bird.style.left = `${birdPosition}px`;
+
+    clouds.style.animation = 'none';
+    clouds.style.left = `${cloudsPosition}px`;
+
+    enemy.style.animation = 'none';
+    enemy.style.left = `${enemyPosition}px`;
+
+    firstGrass.style.animation = 'none';
+    secondGrass.style.animation = 'none';
+
+    //substituição do mario
+    mario.src = 'assets/imgs/mario-game-over.png';
+    mario.style.width = '75px';
+    mario.style.marginLeft = '45px';
+    marioTheme.src = 'assets/audio/smb_gameover.mp3';
+
+    //display elements
+    gameOver.style.display = 'block';
+    restart.style.display = 'block';
+
+    score += 10;
+    scoreElement.innerHTML = score;
+
+    clearInterval(loopGame);
   }
 
   //bloco game-over inimigo
   else if (enemyPosition <= 120 && enemyPosition > 0 && marioPosition < 80) {
-    gameOverLogic();
+    
+    //parando animações
+    pipe.style.animation = 'none';
+    pipe.style.left = `${pipePosition}px`;
+
+    mario.style.animation = 'none';
+    mario.style.bottom = `${marioPosition}px`;
+
+    bigPipe.style.animation = 'none';
+    bigPipe.style.left = `${bigPipePosition}px`;
+
+    bullet.style.animation = 'none';
+    bullet.style.left = `${bulletPosition}px`;
+
+    bulletTwo.style.animation = 'none';
+    bulletTwo.style.left = `${bulletTwoPosition}px`;
+
+    bird.style.animation = 'none';
+    bird.style.left = `${birdPosition}px`;
+
+    clouds.style.animation = 'none';
+    clouds.style.left = `${cloudsPosition}px`;
+
+    enemy.style.animation = 'none';
+    enemy.style.left = `${enemyPosition}px`;
+
+    firstGrass.style.animation = 'none';
+    secondGrass.style.animation = 'none';
+    
+    //substituição do mario
+    mario.src = 'assets/imgs/mario-game-over.png';
+    mario.style.width = '75px';
+    mario.style.marginLeft = '45px';
+    marioTheme.src = 'assets/audio/smb_gameover.mp3';
+
+    //display elements
+    gameOver.style.display = 'block';
+    restart.style.display = 'block';
+
+    score += 1;
+    scoreElement.innerHTML = score;
+    clearInterval(loopGame);
+
   }
 }, 10);
 
@@ -93,9 +216,12 @@ window.addEventListener('touchstart', jump);
 
 //função score
 const marioScore = () => {
+
   const pipePosition = pipe.offsetLeft;
   const bigPipePosition = bigPipe.offsetLeft;
   const enemyPosition = enemy.offsetLeft;
+
+   //console.log(bigPipePosition);
 
   if (pipePosition > 159 && pipePosition < 300) {
     score += 5;
@@ -112,42 +238,3 @@ const marioScore = () => {
 };
 
 document.addEventListener('keydown', marioScore);
-
-function gameOverLogic() {
-  //parando animações
-  let pipePosition = 0;
-  let marioPosition = 0;
-  let bigPipePosition = 0;
-  let bulletPosition = 0;
-  let bulletTwoPosition = 0;
-  let bulletThreePosition = 0;
-  let birdPosition = 0;
-  let cloudsPosition = 0;
-  let enemyPosition = 0;
-
-  pipe.style.animation = 'none';
-  pipe.style.left = `${pipePosition}px`;
-
-  mario.style.animation = 'none';
-  mario.style.bottom = `${marioPosition}px`;
-
-  bigPipe.style.animation = 'none';
-  bigPipe.style.left = `${bigPipePosition}px`;
-
-  bullet.style.animation = 'none';
-  bullet.style.left = `${bulletPosition}px`;
-
-  bulletTwo.style.animation = 'none';
-  bulletTwo.style.left = `${bulletTwoPosition}px`;
-
-  bird.style.animation = 'none';
-  bird.style.left = `${birdPosition}px`;
-
-  clouds.style.animation = 'none';
-  clouds.style.left = `${cloudsPosition}px`;
-
-  enemy.style.animation = 'none';
-  enemy.style.left = `${enemyPosition}px`;
-
-  firstGrass.style.animation = 'none';
-}

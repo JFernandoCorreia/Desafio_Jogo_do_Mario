b2835546894cef65e5ac308fc89eafcd03bb6bf1
JFernando <almo_fernando@hotmail.com>
2024-05-02 14:58:37 -0300

    final

--- js/App.js
+++ js/App.js
@@ -2,5 +2,11 @@ import React from 'react';
 import {WebView} from 'react-native-webview';
 
 export default function App() {
-  return <WebView source={{uri: './play.html'}} />;
+  return (
+    <WebView
+      source={{
+        uri: 'file:///E:/Workspace/Desafio_Mobile/Desafio_Jogo_do_Mario/play.html',
+      }}
+    />
+  );
 }
